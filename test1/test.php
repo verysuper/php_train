c3ae7ac7b80de2429283e749a2a856a26146e61f
verysuper <verysuper@live.com>
2018-05-21 23:36:47 +0800

    echo "<table border='1'>";

--- test1/test.php
+++ test1/test.php
@@ -8,9 +8,10 @@
 
     $category = new Category($db);
     $stmt = $category->read();
-
+    echo "<table border='1'>";
     while ($row_category = $stmt->fetch(PDO::FETCH_ASSOC)){
         extract($row_category);
-        echo "<option value='{$id}'>{$name}</option>";
+        echo "<tr><td>{$id}</td><td>{$name}</td></tr>";
     }
+    echo "</table>";
 ?>
