1b7a345a5d40ca2630de25ea11eab914ca2db2ba
verysuper <verysuper@live.com>
2018-06-03 23:41:45 +0800

    可變變數

---
+++ books/Dev5E/Chapter01/test01.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="" method="post">
+<input name="user"><br>
+<input name="age" ><br>
+<input name="email"><br>
+<input name="cellphone"><br>
+<input type="submit"><br>
+</form>
+</body>
+</html>
+
+<?php
+    foreach($_POST as $key => $value)
+    {    
+        $$key = $value;       
+    }
+    
+    echo '姓名：'.$user;
+    echo '年齡：'.$age;
+    echo '郵箱：'.$email;
+    echo '手機號：'.$cellphone;
+?>
